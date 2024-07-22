230dc53aa6af9b02fbd205cd2824d7b2f2ef275a
Zephyrus <zephyrus1175@yahoo.com>
2024-07-23 01:48:51 +0300

    "Update"

---
+++ logistic_regression.py
@@ -0,0 +1,292 @@
+import numpy as np
+import cvxpy as cp
+import inspect
+import torch
+epsilon = 1e-5
+
+
+class LogisticRegression:
+    def __init__(self, args, X_train, Y_train, X_test):
+        self.X_train = X_train
+        self.Y_train = Y_train
+        self.X_test = X_test
+
+        self.num_samples = self.X_train.shape[0]
+        self.dimension = self.X_train.shape[1]
+
+        self._weights = np.zeros_like(X_train[0])
+        self.lr = args.lr
+        self.optimizer = args.optimizer
+
+        self.gamma = args.gamma
+
+        print("============= CVX solving =============")
+        self.opt_weights, self.opt_obj = self.CVXsolve()
+        print("============= CVX solved =============")
+
+        self.pre_Hessian = self.Hessian(self.weights)
+
+        # Initialize AdamW and SGDW specific variables
+        self.m = np.zeros_like(self.weights)
+        self.v = np.zeros_like(self.weights)
+        self.t = 0
+
+    # UPDATE: Added property decorator for weights.
+    @property
+    def weights(self):
+        return self._weights
+
+    @weights.setter
+    def weights(self, value):
+        if isinstance(value, np.ndarray) and value.shape == self._weights.shape:
+            self._weights = value
+        else:
+            raise ValueError("Invalid value for weights.")
+
+    # sigmoid Function
+    def sigmoid(self, input):
+        input_clipped = np.clip(input, -500, 500) # Avoid overflow
+        assert input_clipped.shape == input.shape, f"input_clipped.shape: {input_clipped.shape}, input.shape: {input.shape}"
+        return 1. / (1. + np.exp(-input_clipped))
+
+    def getTest(self):
+        self.test = self.sigmoid(self.X_test @ self.weights)
+        return self.test
+
+    def objective(self, weights):
+        """
+        return the objective value of the problem
+        note that the objective is averaged over all samples
+        """
+        sigWeights = self.sigmoid(self.X_train @ weights)
+        matGrad = self.Y_train * np.log(sigWeights + epsilon) + (1.0 - self.Y_train) * np.log(1 - sigWeights + epsilon)
+        return - np.sum(matGrad) / self.num_samples + 0.5 * self.gamma * np.linalg.norm(weights) ** 2
+
+    def gradient(self, weights):
+        """
+        return the gradient of objective function
+        note that the gradient is averaged over all samples
+        """
+        sigWeights = self.sigmoid(self.X_train @ weights)
+        matGrad = self.X_train.T @ (sigWeights - self.Y_train)
+        return matGrad / self.num_samples + self.gamma * weights
+
+    def Hessian(self, weights):
+        """
+        return the Hessian of objective function
+        note that the Hessian is averaged over all samples
+        """
+        sigWeights = self.sigmoid(self.X_train @ weights)
+        D_diag = np.diag(sigWeights * (1 - sigWeights))
+        return self.X_train.T @ D_diag @ self.X_train / self.num_samples + self.gamma * np.identity(self.dimension)
+
+    def update(self):
+        # Optimization: Using mapping
+        optimizer_methods = {
+            "GD": self.gradient(self.weights),
+            "ModifiedNewton": self.modified_newton,
+            "ConjugateGradient": self.conjugate_gradient,
+            "LevenbergMarquardt": self.levenberg_marquardt,
+            "BFGS": self.BFGS,
+            "GDArmijo": self.gradient_descent_with_armijo,
+            "ConjugateGDArmijo": self.conjugate_gradient_with_armijo,
+            "ModifiedNewtonArmijo": self.modified_newton_with_armijo,
+            "Adam": self.adam,
+            "AdamW": self.adamw,
+            "SGD": self.sgd,
+            "SGDW": self.sgdw
+        }
+        if self.optimizer == 'GD':
+            gradient = self.gradient(self.weights)
+            update_direction = gradient
+            self.weights -= self.lr * update_direction
+        elif self.optimizer in optimizer_methods:
+            return optimizer_methods[self.optimizer]()
+        else:
+            raise NotImplementedError
+        a, b = self.diff_cal(self.weights)
+
+        return a, b
+
+    def modified_newton(self):
+        gradient = self.gradient(self.weights)
+        hessian = self.Hessian(self.weights)
+
+        update_direction = np.linalg.inv(hessian) @ gradient
+
+        self.weights -= self.lr * update_direction
+
+        a, b = self.diff_cal(self.weights)
+        return a, b
+
+    def conjugate_gradient(self):
+        gradient = self.gradient(self.weights)
+
+        if not hasattr(self, 'last_update'):
+            self.last_update = gradient
+            update_direction = -gradient
+        else:
+
+            beta = np.dot(gradient, gradient) / np.dot(self.last_update, self.last_update)
+            update_direction = -gradient + beta * self.last_update
+            self.last_update = gradient
+
+        self.weights -= self.lr * update_direction
+
+        a, b = self.diff_cal(self.weights)
+        return a, b
+
+    def levenberg_marquardt(self):
+        gradient = self.gradient(self.weights)
+        hessian = self.Hessian(self.weights)
+
+        damping_term = self.gamma * np.identity(self.dimension)
+
+        update_direction = np.linalg.inv(hessian + damping_term) @ gradient
+
+        self.weights -= self.lr * update_direction
+
+        a, b = self.diff_cal(self.weights)
+        return a, b
+
+    def BFGS(self):
+        gradient = self.gradient(self.weights)
+        hessian = self.Hessian(self.weights)
+
+        if not hasattr(self, 'B'):
+            self.B = np.eye(self.dimension)
+            self.last_update = np.zeros_like(gradient)
+            self.last_gradient = np.zeros_like(gradient)
+
+        y = gradient - self.last_gradient
+        s = self.weights - self.last_update
+
+        rho = 1 / np.dot(y, s) if np.dot(y, s) != 0 else 1000
+        V = np.eye(self.dimension) - rho * np.outer(y, s)
+
+        self.B = V.T @ self.B @ V + rho * np.outer(s, s)
+
+        update_direction = -self.B @ gradient
+
+        self.weights -= self.lr * update_direction
+
+        self.last_update = self.weights
+        self.last_gradient = gradient
+
+        a, b = self.diff_cal(self.weights)
+        return a, b
+
+    def gradient_descent_with_armijo(self):
+        gradient = self.gradient(self.weights)
+        direction = -gradient
+        step_size = self.armijo_stepsize_search(gradient, self.weights, direction)
+        self.weights += step_size * direction
+        a, b = self.diff_cal(self.weights)
+        return a, b
+
+    def modified_newton_with_armijo(self):
+        gradient = self.gradient(self.weights)
+        hessian = self.Hessian(self.weights)
+        direction = np.linalg.solve(hessian, gradient)
+        step_size = self.armijo_stepsize_search(gradient, self.weights, direction)
+        self.weights += step_size * direction
+        a, b = self.diff_cal(self.weights)
+        return a, b
+
+    def conjugate_gradient_with_armijo(self):
+        gradient = self.gradient(self.weights)
+        direction = -gradient
+        step_size = self.armijo_stepsize_search(gradient, self.weights, direction)
+        self.weights += step_size * direction
+        a, b = self.diff_cal(self.weights)
+        return a, b
+
+    def armijo_stepsize_search(self, gradient, current_point, direction, alpha=1.0, beta=0.5, max_iter=100, c=0.0001):
+        t = alpha
+        iter_count = 0
+        while iter_count < max_iter:
+            new_point = current_point + t * direction
+            new_value = self.objective(new_point)
+            if new_value <= self.objective(current_point) + c * t * np.dot(gradient, direction):
+                return t
+            else:
+                t *= beta
+                iter_count += 1
+        return t
+
+    def CVXsolve(self):
+        """
+        use CVXPY to solve optimal solution
+        """
+        x = cp.Variable(self.dimension)
+        objective = cp.sum(cp.multiply(self.Y_train, self.X_train @ x) - cp.logistic(self.X_train @ x))
+        prob = cp.Problem(cp.Maximize(objective / self.num_samples - 0.5 * self.gamma * cp.norm2(x) ** 2))
+        prob.solve(solver=cp.ECOS_BB, verbose=False)  # False if not print it
+
+        opt_weights = np.array(x.value)
+        opt_obj = self.objective(opt_weights)
+
+        return opt_weights, opt_obj
+
+    def diff_cal(self, weights):
+        """
+        Calculate the difference of input model weights with optimal in terms of:
+        * 'weights'
+        * 'objective'
+        """
+        weight_diff = np.linalg.norm(weights - self.opt_weights)
+        obj_diff = abs(self.objective(weights) - self.opt_obj)
+        return weight_diff, obj_diff
+
+    # UPDATE: Adam optimizer
+    def adam(self, beta1=0.9, beta2=0.999, epsilon=1e-8):
+        self.t += 1
+        gradient = self.gradient(self.weights)
+        self.m = beta1 * self.m + (1 - beta1) * gradient
+        self.v = beta2 * self.v + (1 - beta2) * (gradient ** 2)
+
+        m_hat = self.m / (1 - beta1 ** self.t)
+        v_hat = self.v / (1 - beta2 ** self.t)
+
+        self.weights -= self.lr * m_hat / (np.sqrt(v_hat) + epsilon)
+
+        a, b = self.diff_cal(self.weights)
+        return a, b
+
+    # UPDATE: AdamW optimizer
+    def adamw(self, beta1=0.9, beta2=0.999, epsilon=1e-8, weight_decay=0.01, device='cpu'):
+        if torch.cuda.is_available() and torch.version.hip is not None:
+            device = 'cuda'
+        
+        self.t += 1
+        gradient = self.gradient(self.weights)
+        self.m = beta1 * self.m + (1 - beta1) * gradient
+        self.v = beta2 * self.v + (1 - beta2) * (gradient ** 2)
+
+        m_hat = self.m / (1 - beta1 ** self.t)
+        v_hat = self.v / (1 - beta2 ** self.t)
+
+        self.weights -= self.lr * m_hat / (np.sqrt(v_hat) + epsilon) + weight_decay * self.weights
+
+        a, b = self.diff_cal(self.weights)
+        fused_aviable = 'fused' in inspect.signature(torch.optim.AdamW).parameters
+        use_fused = fused_aviable and device == 'cuda'
+        print(f"using fused adam: {use_fused}")
+        
+        return a, b
+
+    # UPDATE: SGD optimizer
+    def sgd(self):
+        gradient = self.gradient(self.weights)
+        self.weights -= self.lr * gradient
+
+        a, b = self.diff_cal(self.weights)
+        return a, b
+
+    # UPDATE: SGDW optimizer
+    def sgdw(self, weight_decay=0.01):
+        gradient = self.gradient(self.weights)
+        self.weights -= self.lr * gradient + weight_decay * self.weights
+
+        a, b = self.diff_cal(self.weights)
+        return a, b
---
+++ main.py
@@ -0,0 +1,91 @@
+import sys
+
+sys.path.append('./datasets')
+import os
+import numpy as np
+import pickle as pkl
+from datasets.data_preprocess import data_preprocess
+import logistic_regression
+from options import args_parser
+import datetime
+from plot import plot_logreg
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+current_work_dir = os.path.dirname(__file__)
+
+
+# UPDATE: elapsed_time function.
+def elapsed_time(func):
+    def wrapper(*args, **kwargs):
+        start_time = datetime.datetime.now()
+        start_time.strftime("%H:%M:%S")
+        result = func(*args, **kwargs)
+        end_time = datetime.datetime.now()
+        print("===" * 15)
+        print("Elapsed time: ", end_time - start_time)
+        print("===" * 15)
+        return result
+
+    return wrapper
+
+weight_diff_list = []
+obj_diff_list = []
+args = args_parser()
+@elapsed_time
+def main_run():
+    if __name__ == '__main__':
+        try:
+
+            (x_train, y_train), (x_test, y_test) = data_preprocess(args)
+
+            print('learning rate: ', args.lr)
+            print('Optimizer: ', args.optimizer)
+
+            Model = logistic_regression.LogisticRegression(args=args, X_train=x_train, Y_train=y_train, X_test=x_test)
+
+
+            weight_diff, obj_diff = Model.diff_cal(Model.weights)
+            print("\n------------ Initial ------------")
+            print("weight error: {:.4e}".format(weight_diff))
+            print("objective error: {:.4e}".format(obj_diff))
+
+            Eigvals = np.linalg.eigvals(Model.pre_Hessian)
+            print("\nmax eigenvalue of Hessian:{:.4f}".format(np.max(Eigvals)))
+            print("min eigenvalue of Hessian:{:.4f}".format(np.min(Eigvals)))
+
+            '''
+            update
+            '''
+            for i in range(args.iteration):
+
+                weight_diff, obj_diff = Model.update()
+                print("\n------------ Iteration {} ------------".format(i + 1))
+                print("weight error: {:.4e}".format(weight_diff))
+                print("objective error: {:.4e}".format(obj_diff))
+                weight_diff_list.append(weight_diff)
+                obj_diff_list.append(obj_diff)
+
+                if weight_diff / np.sqrt(Model.dimension) <= 1e-5:
+                    break
+
+            # UPDATE: simplifying the accuracy percent.
+            val = Model.getTest() > 0.5
+            val2 = y_test > 0.5
+            percent_correct = np.mean(val == val2) * 100
+            print(percent_correct, 'Accuracy: %')
+
+
+        except Exception as e:
+            logger.error("An error occurred: %s", e)
+
+
+main_run()
+
+# args = args_parser()
+file_name = './results/{}_{}.pkl'.format('logreg', args.optimizer)
+file_name = os.path.join(current_work_dir, file_name)
+with open(file_name, 'wb') as f:
+    pkl.dump([weight_diff_list, obj_diff_list], f)
+plot_logreg()
